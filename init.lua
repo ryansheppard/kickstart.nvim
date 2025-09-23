5102528cd008ef78693f835aedd55d3ea4624b48
Ryan Sheppard <ryan@chicory.co>
2025-09-23 14:11:49 -0400

    add claude

--- init.lua
+++ init.lua
@@ -635,7 +635,7 @@ require('lazy').setup({
         end,
       })
 
-      require('lspconfig').terraformls.setup {}
+      vim.lsp.config('terraformls', {})
       vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
         pattern = { '*.tf', '*.tfvars' },
         callback = function()
@@ -983,6 +983,30 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    {
+      'coder/claudecode.nvim',
+      dependencies = { 'folke/snacks.nvim' },
+      config = true,
+      keys = {
+        { '<leader>a', nil, desc = 'AI/Claude Code' },
+        { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+        { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+        { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+        { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+        { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+        { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+        { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+        {
+          '<leader>as',
+          '<cmd>ClaudeCodeTreeAdd<cr>',
+          desc = 'Add file',
+          ft = { 'NvimTree', 'neo-tree', 'oil', 'minifiles' },
+        },
+        -- Diff management
+        { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+        { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+      },
+    },
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
